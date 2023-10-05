d58ba997ee08fbfce47d116260501b644fe26b5c
Александр <crazymasterga@gmail.com>
2023-10-05 13:08:48 +0300

    fix

--- Assets/Scripts/Bootstrap.cs
+++ Assets/Scripts/Bootstrap.cs
@@ -1,18 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bootstrap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        CreateWorld();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateWorld()
     {
         
     }
 }
+
+
