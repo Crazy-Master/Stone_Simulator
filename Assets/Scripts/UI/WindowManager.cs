8c42e0b8adb4b69c1d3679309e5e59b0a74f3dac
BlackValet <tatar9927@gmail.com>
2023-10-11 23:48:55 +0300

    button/fix

--- Assets/Scripts/UI/WindowManager.cs
+++ Assets/Scripts/UI/WindowManager.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class WindowManager : MonoBehaviour
 {
     [SerializeField] private GameObject _windowPause;
+    [SerializeField] private GameObject _windowGame;
+
 
 
     public void PauseMenuAct()
     {
         _windowPause.SetActive(true);
+        _windowGame.SetActive(false);
     }
 
     public void PauseMenuDeAct()
     {
         _windowPause.SetActive(false);
+        _windowGame.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("бярюк х бшьек");
     }
 }
